d44d2062ecc2c326b69579469ebfcfa42a7c2b14
cindidwi30 <pramuditacindy30@gmail.com>
2025-06-16 14:48:21 +0700

    Update backend only

--- backend/models/Pembelian.js
+++ backend/models/Pembelian.js
@@ -1,4 +1,33 @@
-//pembelian.js
+// //pembelian.js
+
+// const mongoose = require("mongoose");
+
+// const PembelianSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     paketId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Paket",
+//       required: true,
+//     },
+//     childId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Child",
+//       required: true,
+//     },
+//     tanggalPembelian: {
+//       type: Date,
+//       default: Date.now,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Pembelian", PembelianSchema);
 
 const mongoose = require("mongoose");
 
@@ -22,6 +51,19 @@ const PembelianSchema = new mongoose.Schema(
     tanggalPembelian: {
       type: Date,
       default: Date.now,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+    merchantOrderId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    reference: {
+      type: String,
     },
   },
   { timestamps: true }
