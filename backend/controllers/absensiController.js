d3cc05e4b7f788048229610f8723a2051603875c
cindidwi30 <pramuditacindy30@gmail.com>
2025-06-17 14:23:49 +0700

    Jadwal

--- backend/controllers/absensiController.js
+++ backend/controllers/absensiController.js
@@ -1,100 +1,3 @@
-// //absensiController
-
-// const Absensi = require("../models/Absensi");
-// const Pembelian = require("../models/Pembelian");
-// const { hitungDenda } = require("../utils/hitungDenda");
-
-// exports.hadir = async (req, res) => {
-//   const { childId } = req.body;
-//   const today = new Date();
-//   today.setHours(0, 0, 0, 0);
-
-//   try {
-//     const pembelian = await Pembelian.findOne({ childId }).populate("paketId");
-//     if (!pembelian)
-//       return res.status(404).json({ error: "Paket tidak ditemukan" });
-
-//     let absensi = await Absensi.findOne({ childId, date: today });
-//     if (absensi) return res.status(400).json({ error: "Sudah absen hari ini" });
-
-//     absensi = new Absensi({
-//       childId,
-//       paketId: pembelian.paketId._id,
-//       date: today,
-//       hadirAt: new Date(),
-//     });
-
-//     await absensi.save();
-//     res.json({ message: "Absensi hadir berhasil", absensi });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Gagal menyimpan absensi" });
-//   }
-// };
-
-// // exports.pulang = async (req, res) => {
-// //   const { childId } = req.body;
-// //   const today = new Date();
-// //   today.setHours(0, 0, 0, 0);
-
-// //   try {
-// //     let absensi = await Absensi.findOne({ childId, date: today }).populate(
-// //       "paketId"
-// //     );
-// //     if (!absensi)
-// //       return res.status(404).json({ error: "Belum absen masuk hari ini" });
-
-// //     if (absensi.pulangAt)
-// //       return res.status(400).json({ error: "Sudah pulang" });
-
-// //     const pulangAt = new Date();
-// //     const { lateMinutes, fee } = hitungDenda(absensi.paketId, pulangAt);
-
-// //     absensi.pulangAt = pulangAt;
-// //     absensi.lateDurationMinutes = lateMinutes;
-// //     absensi.lateFee = fee;
-
-// //     await absensi.save();
-// //     res.json({ message: "Absensi pulang berhasil", absensi });
-// //   } catch (err) {
-// //     console.error(err);
-// //     res.status(500).json({ error: "Gagal menyimpan absensi pulang" });
-// //   }
-// // };
-
-// // Tambahkan parameter pulangAt dari req.body untuk testing
-// exports.pulang = async (req, res) => {
-//   const { childId, pulangAt: pulangAtStr } = req.body;
-//   const today = new Date();
-//   today.setHours(0, 0, 0, 0);
-
-//   try {
-//     let absensi = await Absensi.findOne({ childId, date: today }).populate(
-//       "paketId"
-//     );
-//     if (!absensi)
-//       return res.status(404).json({ error: "Belum absen masuk hari ini" });
-
-//     if (absensi.pulangAt)
-//       return res.status(400).json({ error: "Sudah pulang" });
-
-//     // Gunakan waktu pulang dari payload jika ada, default ke waktu sekarang
-//     const pulangAt = pulangAtStr ? new Date(pulangAtStr) : new Date();
-
-//     const { lateMinutes, fee } = hitungDenda(absensi.paketId, pulangAt);
-
-//     absensi.pulangAt = pulangAt;
-//     absensi.lateDurationMinutes = lateMinutes;
-//     absensi.lateFee = fee;
-
-//     await absensi.save();
-//     res.json({ message: "Absensi pulang berhasil", absensi });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Gagal menyimpan absensi pulang" });
-//   }
-// };
-
 const Absensi = require("../models/Absensi");
 const Pembelian = require("../models/Pembelian");
 const { hitungDenda } = require("../utils/hitungDenda");
@@ -158,11 +61,25 @@ exports.pulang = async (req, res) => {
   }
 };
 
+// exports.getAllAbsensi = async (req, res) => {
+//   try {
+//     const absensi = await Absensi.find()
+//       .populate("childId") // tambahkan ini untuk menampilkan data anak
+//       .sort({ date: -1 }); // urutkan dari yang terbaru
+
+//     res.json(absensi);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: "Gagal mengambil data absensi" });
+//   }
+// };
+
 exports.getAllAbsensi = async (req, res) => {
   try {
     const absensi = await Absensi.find()
-      .populate("childId") // tambahkan ini untuk menampilkan data anak
-      .sort({ date: -1 }); // urutkan dari yang terbaru
+      .populate("childId")
+      .populate("paketId") // <-- penting agar semua field absensi lengkap
+      .sort({ date: -1 });
 
     res.json(absensi);
   } catch (err) {
--- backend/models/Absensi.js
+++ backend/models/Absensi.js
@@ -1,28 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const absensiSchema = new mongoose.Schema({
-//   childId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Child",
-//     required: true,
-//   },
-//   paketId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Paket",
-//     required: true,
-//   },
-//   date: {
-//     type: Date,
-//     required: true,
-//   },
-//   hadirAt: Date,
-//   pulangAt: Date,
-//   lateDurationMinutes: Number,
-//   lateFee: Number,
-// });
-
-// module.exports = mongoose.model("Absensi", absensiSchema);
-
 const mongoose = require("mongoose");
 
 const absensiSchema = new mongoose.Schema({
