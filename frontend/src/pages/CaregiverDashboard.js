351422417962f80d4336b248838d539575539ace
cindidwi30 <pramuditacindy30@gmail.com>
2025-06-17 18:16:15 +0700

    Jadwal

--- frontend/src/pages/CaregiverDashboard.js
+++ frontend/src/pages/CaregiverDashboard.js
@@ -1,23 +1,54 @@
-import React from "react";
-import Sidebar from "../components/caregiver/dashboard/PengasuhSidebar";
-import Navbar from "../components/caregiver/dashboard/PengasuhNavbar";
+// import React from "react";
+// import Sidebar from "../components/caregiver/dashboard/PengasuhSidebar";
+// import Navbar from "../components/caregiver/dashboard/PengasuhNavbar";
+// import { Outlet } from "react-router-dom";
+
+// function ParentDashboard() {
+//   return (
+//     <div className="d-flex">
+//       <Sidebar />
+//       <div
+//         className="flex-grow-1 bg-white min-vh-100 d-flex flex-column"
+//         style={{ marginLeft: "250px" }}
+//       >
+//         <Navbar />
+//         <div className="container mt-4">
+//           <Outlet /> {/* Ini yang akan render Summary atau Biodata */}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ParentDashboard;
+
+//caregiver.js
+
+import React, { useState } from "react";
+import Sidebar from "../components/caregiver/layout/Sidebar";
+import Navbar from "../components/caregiver/layout/Navbar";
 import { Outlet } from "react-router-dom";
 
-function ParentDashboard() {
+const CaregiverDashboard = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
   return (
     <div className="d-flex">
-      <Sidebar />
+      {/* Sidebar */}
+      {showSidebar && <Sidebar />}
+
       <div
-        className="flex-grow-1 bg-white min-vh-100 d-flex flex-column"
-        style={{ marginLeft: "250px" }}
+        className="flex-grow-1"
+        style={{ marginLeft: showSidebar ? 250 : 0 }}
       >
-        <Navbar />
-        <div className="container mt-4">
-          <Outlet /> {/* Ini yang akan render Summary atau Biodata */}
+        {/* Navbar */}
+        <Navbar toggleSidebar={() => setShowSidebar(!showSidebar)} />
+        <div className="p-3">
+          <Outlet />
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default ParentDashboard;
+export default CaregiverDashboard;
